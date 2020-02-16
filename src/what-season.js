a7766ce6777b5ac8f475b392492f9b20ffed3824
803142 <803142@gmail.com>
2020-02-16 20:35:19 +0300

    feat: season was found

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,24 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date){
+    return 'Unable to determine the time of year!';
+  };
+  if (Object.prototype.toString.call(date) === '[object Date]'){
+    const seasons = {
+      'Jan': 'winter',
+      'Dec': 'winter',
+      'Feb': 'winter',
+      'Mar': 'spring',
+      'Apr': 'spring',
+      'May': 'spring',
+      'Jun': 'summer',
+      'Jul': 'summer',
+      'Aug': 'summer',
+      'Sep': 'fall',
+      'Oct': 'fall',
+      'Nov': 'fall',
+    };
+    return seasons[date.toString().split(' ')[1]];
+  } else {
+    throw new Error()
+  };
 };
