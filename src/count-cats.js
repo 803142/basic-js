8ed14d1f2aa91dcd3a51ca00e8f5fc6b807f62d4
803142 <803142@gmail.com>
2020-02-16 17:14:49 +0300

    feat: cats was found

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,8 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  return matrix.reduce((acc,cury)=>{
+    return acc+cury.reduce((accy, curx)=>{
+      return curx==="^^"? accy = accy + 1: accy
+    },0)
+  },0
+  )
 };
