b3856e8e54de6082ede57df528f436c58aa6d5fe
803142 <803142@gmail.com>
2020-02-17 02:41:28 +0300

    feat: chain created

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let message = ''+value;    
+    this.chain? this.chain.push(message):this.chain = [message];
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (this.chain.length-1<position || position<1 || typeof position !== 'number'){
+      this.chain = [];
+       throw new Error();
+      }else{        
+        this.chain.splice(position-1,1);        
+        return this;
+      } 
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = '( '+this.chain.join(' )~~( ')+' )';
+    this.chain = [];
+    return result;
   }
 };
 
