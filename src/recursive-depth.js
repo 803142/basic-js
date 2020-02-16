b04d503d1a13f31c54102c1b9a6e2d86f57cc2aa
803142 <803142@gmail.com>
2020-02-16 22:48:46 +0300

    feat: depth is find

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,10 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (Array.isArray(arr)){ 
+                let result = arr.map(cur=>this.calculateDepth(cur)).sort((a,b)=>b-a)[0]||0
+                return result + 1;               
+        } else {
+                return 0;
+        }
     }
 };
