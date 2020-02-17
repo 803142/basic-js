27125d4ec571a8357f48f61dbb7bc4bec0d12fcd
803142 <803142@gmail.com>
2020-02-17 04:13:39 +0300

    feat: age found

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity||typeof sampleActivity !=='string'||sampleActivity.match(/(\D)+[.]?(\D)+/g)){
+    return false;
+  } else {
+    let numericActivity = parseFloat(sampleActivity);
+    if (numericActivity>15||numericActivity<=0){
+      return false;
+    } else {
+      return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+    }
+  }
 };
