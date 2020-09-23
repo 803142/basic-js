3e526ae173c6112b5a9b28b7933d0f86af025235
803142 <803142@gmail.com>
2020-09-23 14:44:28 +0300

    feat: venegar cyfer

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,41 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor (forward = true) {
+        this.forward = forward;
+        this.abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key) throw Error();
+		let cypher = '';
+        let messageReady = message.toUpperCase();
+        let keyReady = key.toUpperCase();
+        let nonABC = 0;
+		for(let i =0; i<messageReady.length; i++) {
+			if (!messageReady[i].match(/[A-Z]/)) {
+                nonABC += 1;
+				cypher += messageReady[i];
+			} else {
+                cypher += this.abc.charAt((this.abc.indexOf(messageReady.charAt(i)) + this.abc.indexOf( keyReady.charAt((i-nonABC) % keyReady.length))) % this.abc.length)
+			}
+        }
+        return this.forward? cypher : cypher.split('').reverse().join('');
+	}
+
+    decrypt(message, key) {
+        if (!message || !key) throw Error();
+        let cypher = '';
+        let messageReady = message.toUpperCase();
+        let keyReady = key.toUpperCase();
+        let nonABC = 0;
+        for (let i=0; i < messageReady.length; i+=1 ) {
+            if (!messageReady[i].match(/[A-Z]/)) {
+                nonABC += 1;
+                cypher += messageReady[i];
+            } else {
+                cypher += this.abc.charAt(((this.abc.indexOf(messageReady.charAt(i))+ this.abc.length) - this.abc.indexOf( keyReady.charAt((i-nonABC) % keyReady.length))) % this.abc.length)
+            }
+        }
+        return this.forward? cypher : cypher.split('').reverse().join('');
     }
 }
 
