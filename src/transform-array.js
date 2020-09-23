7785d6120a124f3a2a817e8a5359676054a0f452
803142 <803142@gmail.com>
2020-09-23 10:01:56 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error();
+    const result = [];
+    let discard = null;
+
+    for (let i = 0; i < arr.length; i+=1) {
+        const conditionPrev = !!(result.length && discard !== i - 1);
+        const conditionNext = !!(i < arr.length - 1);
+        switch(arr[i]) {
+            case '--discard-next':
+              discard = i + 1;
+              i+=1;
+              break;
+            case '--discard-prev':
+              if (conditionPrev) result.pop(arr[i]);
+              break;
+            case '--double-next':
+              if (conditionNext) result.push(arr[i+1]);
+              break;
+            case '--double-prev':
+              if (conditionPrev) result.push(result[result.length-1]);
+              break;
+            default:
+              result.push(arr[i]);
+          }
+    }
+    return result;
 };
